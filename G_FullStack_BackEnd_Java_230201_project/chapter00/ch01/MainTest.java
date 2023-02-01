4d2958795c21510d99b235443705f9fa4f0cbbbc
afeelingeffort <105858187+afeelingeffort@users.noreply.github.com>
2023-02-01 17:36:46 +0900

    Add files via upload

---
+++ G_FullStack_BackEnd_Java_230201_project/chapter00/ch01/MainTest.java
@@ -0,0 +1,13 @@
+package ch01;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		System.out.println("반가워~");
+		
+//		데이터를 어딘가 저장하고 싶다면 변수를 쓰면 된다.
+		
+//		변수에 어떤 형태의 데이터 타입인지 명시하는 것이 자료형이라고 한다
+	}
+
+}
---
+++ G_FullStack_BackEnd_Java_230201_project/chapter00/ch01/MainTest2.java
@@ -0,0 +1,7 @@
+package ch01;
+
+public class MainTest2 {
+	public static void main(String[] args) {
+		System.out.println("hello ~~hi~");
+	}
+}
