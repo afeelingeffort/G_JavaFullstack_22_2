8f9e01e440314460d574773ef15d877fd22a5fda
afeelingeffort <105858187+afeelingeffort@users.noreply.github.com>
2023-02-16 16:51:26 +0900

    Add files via upload

---
+++ G_FullStack_BackEnd_Java_230216_project/generic_ex/ch01/MainTest1.java
@@ -0,0 +1,28 @@
+package ch01;
+
+public class MainTest1 {
+
+	// main 함수
+	public static void main(String[] args) {
+
+		// 무엇이든 담을 수 있는 제네릭 프로그래밍
+
+		// 제네릭 프로그래밍을 사용하는 이유
+		// 우리가 변수를 사용한다고 하면 변수에 항상 자료형을 먼저 지정하게 되어있다.
+		// 하지만 같은 변수 이름이고 데이터 타입만 달라지는 경우를 만난다면
+		// 제네릭 프로그래밍을 생각하자 !
+
+		// ex)
+		// double result;
+		// int result;
+		// float result;
+		// --> a.java, b.java, c.java <--따로 만들어야 한다.
+		// 제네릭을 이용하면 위와 같은 상황을 해결할 수 있다.
+
+		// 결론
+		// 다양한 자료형이 적용될 수 있는 클래스를 만드는 것이
+		// 제네릭 프로그래밍 방식이다.
+
+	} // end of main
+
+} // end of class
