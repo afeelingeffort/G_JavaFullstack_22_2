d11b13036ccd5ec41afac2a71d7be225614c5a59
afeelingeffort <105858187+afeelingeffort@users.noreply.github.com>
2023-02-21 17:11:01 +0900

    Add files via upload

---
+++ G_FullStack_BackEnd_Java_230221_project/bubble/ex07/Moveable.java
@@ -0,0 +1,12 @@
+package ex07;
+
+public interface Moveable {
+	public abstract void left();
+	public abstract void right();
+	public abstract void up();
+
+	// default 문법 사용해보기
+	// 후크 메서드 (구현부만 있고 로직은 없다.)
+	// Moveable 구현하는 클래스에서 필요하다면 재정의해서 사용해
+	default public void down() {};
+}
---
+++ G_FullStack_BackEnd_Java_230221_project/bubble/ex07/PlayerWay.java
@@ -0,0 +1,8 @@
+package ex07;
+
+// enum 타입을 사용하는 이유 - 가독성 
+// PlayerWay.LEFT, PlayerWay.RIGHT
+// 데이터 범주화가 필요하다면 enum 타입을 사용할 수 있다. 
+public enum PlayerWay {
+	LEFT, RIGHT
+}
