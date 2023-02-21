f5156d3293bcee485013ab531a406bddc29c69d6
afeelingeffort <105858187+afeelingeffort@users.noreply.github.com>
2023-02-21 17:10:23 +0900

    Add files via upload

---
+++ G_FullStack_BackEnd_Java_230221_project/bubble/ex06/Bubble.java
@@ -0,0 +1,116 @@
+package ex06;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Bubble extends JLabel implements Moveable {
+
+	// 버블에 위치 상태
+	private int x;
+	private int y;
+
+	// 버블에 움직임 방향 상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+
+	// 적군을 맞춘 상태 : 0 기본, 1, (적을 가둔 물방울)
+	private int state;
+
+	private ImageIcon bubble; // 기본 물방울 이미지
+	private ImageIcon bubbled; // 적을 가둔 상태 이미지
+	private ImageIcon bomb; // 물방울이 터진 상태 이미지
+
+	private Player player;
+
+	// 버블을 Player에 의존하고 있다.
+	public Bubble(Player player) {
+		this.player = player;
+		initData();
+		setInitLayout();
+		initThread();
+	}
+
+	private void initData() {
+		bubble = new ImageIcon("images/bubble.png");
+		bubbled = new ImageIcon("images/bubled.png");
+		bomb = new ImageIcon("images/bomb.png");
+		left = false;
+		right = false;
+		up = false;
+		state = 0;
+	}
+
+	private void setInitLayout() {
+		// 플레이어가 있는 위치에 태어나야 한다.
+		x = player.getX();
+		y = player.getY();
+		setIcon(bubble);
+		setSize(50, 50);
+		setLocation(x, y);
+	}
+
+	private void initThread() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				if (player.getpWay() == PlayerWay.LEFT) {
+					// 왼쪽 방향을 보고 있다.
+					left();
+				} else {
+					right();
+				}
+
+			}
+		}).start();
+	}
+
+	@Override
+	public void left() {
+		left = true;
+		for (int i = 0; i < 400; i++) {
+			x--;
+			setLocation(x, y);
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} // end of for
+		up();
+
+	}
+
+	@Override
+	public void right() {
+		right = true;
+		for (int i = 0; i < 400; i++) {
+			x++;
+			setLocation(x, y);
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} // end of for
+		up();
+
+	}
+
+	@Override
+	public void up() {
+		up = true;
+
+		while (true) {
+			y--;
+			setLocation(x, y);
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ G_FullStack_BackEnd_Java_230221_project/bubble/ex06/Player.java
@@ -0,0 +1,209 @@
+package ex06;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Player extends JLabel implements Moveable {
+	
+	private int x; 
+	private int y; 
+	private ImageIcon playerR, playerL;
+	
+	// 플레이어에 방향 상태(enum 타입 사용: 데이터의 범주화) 
+	private PlayerWay pWay; 
+	
+	// 플레이어에 현재 움직임 상태를 기록 
+	private boolean left; 
+	private boolean right; 
+	private boolean up; 
+	private boolean down; 
+	
+	// 벽에 충돌한 상태 
+	private boolean leftWallCrash; 
+	private boolean rightWallCrash; 
+	
+	// 플레이어에 속도 상태 
+	private final int SPEED = 4; 
+	private final int JUMPSPEED = 2; 
+	
+	// setter 메서드 만들기 left, right 
+	
+	
+	public Player() {
+		initData();
+		setInitLayout();
+	}
+	
+	public PlayerWay getpWay() {
+		return pWay;
+	}
+
+	public void setpWay(PlayerWay pWay) {
+		this.pWay = pWay;
+	}
+
+	// setter method 
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+	
+	public void setRight(boolean right) {
+		this.right = right;
+	}
+
+	public boolean isLeftWallCrash() {
+		return leftWallCrash;
+	}
+
+	public void setLeftWallCrash(boolean leftWallCrash) {
+		this.leftWallCrash = leftWallCrash;
+	}
+
+	public boolean isRightWallCrash() {
+		return rightWallCrash;
+	}
+
+	public void setRightWallCrash(boolean rightWallCrash) {
+		this.rightWallCrash = rightWallCrash;
+	}
+	
+	public boolean isLeft() {
+		return left;
+	}
+
+	public boolean isRight() {
+		return right;
+	}
+	
+	public boolean isUp() {
+		return up;
+	}
+
+	public void setUp(boolean up) {
+		this.up = up;
+	}
+
+	public boolean isDown() {
+		return down;
+	}
+
+	public void setDown(boolean down) {
+		this.down = down;
+	}
+
+	private void initData() {
+		playerR = new ImageIcon("images/playerR.png");
+		playerL = new ImageIcon("images/playerL.png");
+		
+		left = false; 
+		right = false; 
+		up = false; 
+		down = false; 
+		leftWallCrash = false; 
+		rightWallCrash = false; 
+		pWay = PlayerWay.RIGHT;
+	}
+	
+	private void setInitLayout() {
+		x = 500; 
+		y = 535; 
+		// 좌표기반, 라벨에 크기를 지정해야 한다. 
+		setSize(50, 50);
+		setLocation(x, y);
+		// JLabel 에 아이콘을 셋팅하는 메서드 
+		setIcon(playerR);
+	}
+
+	@Override
+	public void left() {
+		pWay = PlayerWay.LEFT; // 2가지 
+		left = true; 
+		// 한번 키보드 왼쪽 방향키를 누르면 
+		// 쓰레드 생성이 된다. 
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(left) {
+					setIcon(playerL);
+					x = x - SPEED; 
+					setLocation(x, y);
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				} // end of while 
+			}
+		}).start();
+	}
+
+	@Override
+	public void right() {
+		pWay = PlayerWay.RIGHT;
+		right = true; 
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(right) {
+					setIcon(playerR);
+					x = x + SPEED; 
+					setLocation(x, y);
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				} // end of while
+			}
+		}).start();
+	}
+
+	@Override
+	public void up() {
+		System.out.println("점프 ! ");
+		up = true; 
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for(int i = 0; i < 130 / JUMPSPEED; i++) {
+					y = y - JUMPSPEED;
+					setLocation(x, y);
+					try {
+						Thread.sleep(5);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				} // end of for 
+				
+				up = false; 
+				down();
+				
+			}
+		}).start();
+	}
+
+	@Override
+	public void down() {
+		down = true; 
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(down) {
+					y = y + JUMPSPEED;
+					setLocation(x, y);
+					down = false;
+					
+					try {
+						Thread.sleep(3);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				} // end of while
+			
+			}
+		}).start();
+	}
+	
+	
+	
+}
