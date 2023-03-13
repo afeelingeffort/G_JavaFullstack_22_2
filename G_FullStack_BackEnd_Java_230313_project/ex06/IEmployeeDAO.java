76b3018deff2b3c5681ee1f65e7679c29cf59532
afeelingeffort <105858187+afeelingeffort@users.noreply.github.com>
2023-03-13 21:57:31 +0900

    Delete G_FullStack_BackEnd_Java_230313_project directory

--- G_FullStack_BackEnd_Java_230313_project/ex06/IEmployeeDAO.java
+++
@@ -1,9 +0,0 @@
-package ex06;
-
-import java.util.ArrayList;
-
-public interface IEmployeeDAO {
-	
-	// 해당 직함의 이름을 입력 받아 직원을 출력
-	ArrayList<EmployeeDTO> showTitleEmpInfo(String title);
-}
--- G_FullStack_BackEnd_Java_230313_project/ex07me/ITravelDAO.java
+++
@@ -1,9 +0,0 @@
-package ex07me;
-
-import java.util.ArrayList;
-
-public interface ITravelDAO {
-	
-	//성이 김씨인 사람이 여행 간 곳과 날짜를 출력
-	ArrayList<TravelDTO> showTravelInfo(String name);
-}
